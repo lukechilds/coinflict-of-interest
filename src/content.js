eb44e4782c84f1c57a60740a10da027e27d97246
Luke Childs <lukechilds123@gmail.com>
2019-03-20 14:50:46 +0700

    Insert biases to profile card

--- src/content.js
+++ src/content.js
@@ -1,1 +1,22 @@
-console.log('coinbias');
+const injectChart = () => {
+	const profileHoverContainer = document.querySelector('#profile-hover-container');
+	const profileCard = profileHoverContainer.querySelector('.profile-card');
+
+	if (!(profileCard && !profileCard.dataset.coinbias)) {
+		return;
+	}
+	profileCard.dataset.coinbias = true;
+
+	const biases = document.createElement('div');
+	biases.innerHTML = `
+	<div class="ProfileCardStats">
+		<span class="ProfileCardStats-statLabel u-block">Bias</span>
+		[charts]
+	</div>`;
+
+	const profileCardStats = profileCard.querySelector('.ProfileCardStats');
+	profileCardStats.parentNode.insertBefore(biases, profileCardStats);
+};
+
+observer = new MutationObserver(injectChart);
+observer.observe(document.body, {childList: true, subtree: true});
