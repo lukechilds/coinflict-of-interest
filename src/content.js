89cfdca479a7c0731ad773f885bc96b8a08836c5
Luke Childs <lukechilds123@gmail.com>
2019-03-20 15:19:21 +0700

    Cache API responses

--- src/content.js
+++ src/content.js
@@ -1,12 +1,22 @@
+const dataCache = new Map();
+
 const getData = async username => {
 	const url = `https://hive.one/api/top-people/${username}`;
+
+	const cachedData = dataCache.get(url);
+	if (typeof cachedData !== 'undefined') {
+		return cachedData;
+	}
+
 	const response = await fetch(url);
 
 	if (response.status === 404) {
+		dataCache.set(url, false);
 		return false;
 	}
 
 	const data = await response.json();
+	dataCache.set(url, data);
 	return data;
 };
 
