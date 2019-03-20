62302ff45fbb64923afb322d17649862316e607b
Luke Childs <lukechilds123@gmail.com>
2019-03-20 15:19:07 +0700

    Fetch data from API

--- src/content.js
+++ src/content.js
@@ -1,4 +1,16 @@
-const injectChart = () => {
+const getData = async username => {
+	const url = `https://hive.one/api/top-people/${username}`;
+	const response = await fetch(url);
+
+	if (response.status === 404) {
+		return false;
+	}
+
+	const data = await response.json();
+	return data;
+};
+
+const injectChart = async () => {
 	const profileHoverContainer = document.querySelector('#profile-hover-container');
 	const profileCard = profileHoverContainer.querySelector('.profile-card');
 
@@ -7,12 +19,21 @@ const injectChart = () => {
 	}
 	profileCard.dataset.coinbias = true;
 
-	const biases = document.createElement('div');
-	biases.innerHTML = `
-	<div class="ProfileCardStats">
+	const username = profileCard.querySelector('[data-screen-name]').dataset.screenName;
+	const data = await getData(username);
+
+	const container = document.createElement('div');
+	container.innerHTML = `
+	<div class="ProfileCardBias ProfileCardStats">
 		<span class="ProfileCardStats-statLabel u-block">Bias</span>
-		[charts]
 	</div>`;
+	const biases = container.children[0];
+
+	if (data) {
+		biases.appendChild(document.createTextNode(':)'));
+	} else {
+		biases.appendChild(document.createTextNode('No bias data available for this user.'));
+	}
 
 	const profileCardStats = profileCard.querySelector('.ProfileCardStats');
 	profileCardStats.parentNode.insertBefore(biases, profileCardStats);
